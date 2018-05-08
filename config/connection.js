20b4d385a5ede7f2f68b024cef169efe94a98c2f
MollyLJohnson <mollyl.johnson@att.net>
2018-05-08 16:37:43 -0500

    added JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 require('dotenv').config();
 
 var mysql = require("mysql");
-
-var connection = mysql.createConnection({
+var connection;
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} 
+else {
+  connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: "burgerdb"
-});
+  });
+}
 
 connection.connect(function(err) {
   if (err) {
