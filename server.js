dc07968b8b8f47f0785a2f5077f2113cd6dc4e44
-MJ <mollyl.johnson@att.net>
2018-11-06 16:07:43 -0600

    updated packages and took out bodyParser package

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var path = require("path");
+const express = require("express");
+const path = require("path");
 
-var PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
-var app = express();
+const app = express();
 app.use(express.static(path.join(__dirname, "/public")));
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-var handle = require("express-handlebars");
+const handle = require("express-handlebars");
 
 app.engine("handlebars", handle({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var router = require("./controllers/burgers_controller.js");
+const router = require("./controllers/burgers_controller.js");
 app.use(router);
 
 app.listen(PORT, function() {
